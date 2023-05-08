380b57bf195f51ae15a3b6011d39fda3749de3ab
Gaspar E Martin <gasparmartin937@gmail.com>
2023-05-07 22:30:06 -0300

    git init
    git init

---
+++ Clase 17/ejemplo.js
@@ -0,0 +1,13 @@
+
+// estos son los posibles valores de la variable booliana
+
+console = true; //verdadero
+console = false; //falso
+
+// operadores unitarios
+
+++ numero; //le sumo antes de leerlo
+numero ++; //leo y luego sumo
+
+-- numero; //resto y luego leo
+numero --; // resto y luego leo
