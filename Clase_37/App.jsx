fb844b37f7c4af4405595832a3266d0e3e6e33bc
Gaspar E Martin <gasparmartin937@gmail.com>
2023-09-26 17:31:24 -0300

    Probando React

---
+++ Clase_37/App.jsx
@@ -0,0 +1,8 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+
+import Menu from './Menu.jsx'
+import Home from './Home.jsx'
+import Login from './Login.jsx'
+import GiftCards from './GiftCards.jsx'
+import Error404 from './Error404.jsx'
+
