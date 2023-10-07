ef77a493e70710d9a4e98cc30e3100f928e6f8b8
Gaspar E Martin <gasparmartin937@gmail.com>
2023-10-06 21:03:39 -0300

    creacion de nuevo react

--- Modulo 3/Clase 36/componente-props/src/components/navbar/Navbar.jsx
+++ Modulo 3/Clase 36/componente-props/src/components/navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import {nav} from './navbar.module.css'
 const Navbar = ({user, setUser}) => {
-    const {name, lastname} = user;
+    const { name, lastname } = user;
 
     const [changeMenu, setchangeMenu] = useState('Menu')
 
     const updateUserName = () => {
         setUser({...user, name: 'Enrique'})
-    }
+    };
 
     return (
     <nav className={nav}>
         <ul>
-            <li>Menu</li>
+            <li>{changeMenu}</li>
             <li>Contacto</li>
             <li>Login</li>
 
-            <button onClick={() => updateUserName}>
+            <button onClick={() => updateUserName()}>
                 Actualizar nombre
             </button>
 
